b71b2c59bcf33a21f7880c069d2431d12982bd47
KPSPDK <73800985+KPSPDK@users.noreply.github.com>
2021-01-30 21:47:45 +0300

    Тест формы обратной связи

---
+++ Project/Pages/mailer/smart.php
@@ -0,0 +1,46 @@
+<?php 
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$site = $_POST['user_site'];
+$message = $_POST['user_message'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = '	ssl://smtp.yandex.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 't3stovtesttestov@yandex.ru';                 // Наш логин
+$mail->Password = 'Kirill200521@';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('t3stovtesttestov@yandex.ru', 't3stovtesttestov');   // От кого письмо 
+$mail->addAddress('kirya.ignatev.gg@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Это тема сообщения';
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: ' . $name . ' <br>
+	Телефон: ' . $phone . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
