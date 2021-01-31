11e4d9b901c001627995f130700777a4b441bf9e
KPSPDK <73800985+KPSPDK@users.noreply.github.com>
2021-01-31 17:38:41 +0300

    Add files via upload

---
+++ Pols/script.js
@@ -0,0 +1,17 @@
+ function fun1() {
+ //rtl - радиус скругления верхний левый угол
+ var rtl=document.getElementById('rtl').value;
+ //rtr - радиус скругления верхний правый угол
+ var rtr=document.getElementById('rtr').value;
+ //rbr - радиус скругления нижний правый угол
+ var rbr=document.getElementById('rbr').value;
+ //rbl - радиус скругления верхний правый угол
+ var rbl=document.getElementById('rbl').value;
+ //Теперь получим элементы input type="text"
+ var ttl=document.getElementById('ttl');
+ var ttr=document.getElementById('ttr');
+ var tbr=document.getElementById('tbr');
+ var tbl=document.getElementById('tbl');
+ // получаем div по id
+ var block=document.getElementById('block');
+ }
